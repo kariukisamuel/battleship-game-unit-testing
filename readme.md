b27534489c20abb5c1205ce225c9b0630126393e
samuel <inboxsamuel@outlook.com>
2021-12-22 13:47:42 +0300

    First Commit

---
+++ readme.md
@@ -0,0 +1,12 @@
+# Battleship Game Unit Testing
+
+## Build Setup
+
+```bash
+# install dependencies
+$ npm install
+
+# to test
+$ npm test
+
+```
---
+++ test/main_test.js
@@ -0,0 +1,8 @@
+var expect = require("chai").expect;
+//Test Suite 
+describe("Mocha", function(){
+   //Test Spec (unit test)
+   it("should run our test using npm", function(){
+           expect(true).to.be.ok;
+   })
+})
