94d113883444e4d5aaa9e6325089438f8043a601
samuel <inboxsamuel@outlook.com>
2021-12-22 15:38:19 +0300

    Testing Damage Ship Function

--- test/ship_test.js
+++ test/ship_test.js
@@ -1,8 +1,9 @@
 var expect = require('chai').expect;
-
+//Test Suite
 describe('checkForShip ', function () {
-    var checkForShip = require('../game_logic/ship_methods').checkForShip;
 
+    var checkForShip = require('../game_logic/ship_methods').checkForShip;
+    //test spec
     it('should correctly report no ship at a given players coordinate', function () {
         player = {
             ships: [{
@@ -13,6 +14,7 @@ describe('checkForShip ', function () {
         }
         expect(checkForShip(player, [9, 9])).to.be.false;
     })
+    //test spec
     it('should correctly report ship at a given players coordinate', function () {
         player = {
             ships: [{
@@ -24,6 +26,7 @@ describe('checkForShip ', function () {
         expect(checkForShip(player, [0, 0])).to.be.true;
 
     })
+    //test spec
     it('should handle ships at more than one coordinate', function () {
         player = {
             ships: [{
@@ -38,6 +41,7 @@ describe('checkForShip ', function () {
         expect(checkForShip(player, [9, 9])).to.be.false;
 
     })
+    //test spec
     it('should handle checking multiple ships', function () {
         player = {
             ships: [{
@@ -67,6 +71,21 @@ describe('checkForShip ', function () {
         expect(checkForShip(player, [2, 1])).to.be.true;
         expect(checkForShip(player, [9, 9])).to.be.false;
 
+    })
+
+})
+
+describe('damageShip', function (){
+    var damageShip = require('../game_logic/ship_methods').damageShip;
+    it('should register damage on a ship on a given location',function(){
+      var ship ={
+        locations:[[0,0]],
+        damage: []
+      }
+     damageShip(ship,[0,0])
+     expect(ship.damage).to.not.be.empty
+     expect(ship.damage[0]).to.deep.equal([0,0])
+  
     })
 
 })
